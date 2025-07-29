6a88aa725c28eacfe73e24f07c0ba5003314697c
kyoko K <kyoko@kyokonoMacBook-Air.local>
2025-07-29 09:06:10 +0900

    AuthGuardの設定

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FirebaseAuthGuard } from './firebase-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(FirebaseAuthGuard)
+  @Get("/hello")
+  getHello(): {data: string} {
+    const hello = this.appService.getHello()
+    return {data: hello};
   }
 }
