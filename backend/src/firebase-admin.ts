cad4772dd863bfb0ac329ee092fcb597822e64d9
kyoko K <kyoko@kyokonoMacBook-Air.local>
2025-07-29 09:06:01 +0900

    envに入れたものは自動でfirebase-adminで取得される

--- backend/src/firebase-admin.ts
+++ backend/src/firebase-admin.ts
@@ -3,9 +3,7 @@ import * as admin from 'firebase-admin';
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY_JSON || '{}');
 
 // 初期化
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+admin.initializeApp();
 
 /**
  * トークン(JWD)を検証する
